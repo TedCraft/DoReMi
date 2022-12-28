030a2860aaf54732c12f4e07c7461192e9a00727
TedCraft <81485481+TedCraft@users.noreply.github.com>
2022-12-28 04:24:28 +0300

    initial commit

---
+++ DOREMI.js
@@ -0,0 +1,349 @@
+class ServiceVariables {
+    constructor(input = []) {
+        this._stack = {};
+        this._index = 0;
+        this.output = [];
+        this._input = input;
+        this._inputIndex = 0;
+        this.iteration = 0;
+        this.returnTo = [];
+        this.jumpTo = [];
+        this.skip = false;
+        this.skipCondition = { statement: false, count: 0 };
+    }
+
+    get index() { return this._index }
+    set index(i) {
+        if (i < 0) throw new RangeError(`Index cannot be under 0!`);
+        this._index = i;
+    }
+
+    get stack() {
+        return this._stack[this._index] || 0;
+    }
+    set stack(value) {
+        value != 0 ? this._stack[this._index] = value : delete this._stack[this._index];
+    }
+    get next() {
+        return this._stack[this._index + 1] || 0;
+    }
+
+    stackWithOffset(offset) {
+        return this._stack[this._index + offset] || 0;
+    }
+
+    get input() {
+        this._inputIndex++;
+        return this._input[this._inputIndex - 1] || 0;
+    }
+
+    return() {
+        this.iteration = (this.returnTo.pop() || this.iteration) - 1;
+    }
+
+    jump() {
+        if (this.jumpTo[0])
+            this.iteration = this.jumpTo.shift();
+        else this.skip = true;
+    }
+}
+
+class Note {
+    constructor(note, additions) {
+        Note.checkFormatNote(note, additions.alteration, additions.mood, additions.octave);
+        this.note = note;
+        this.alteration = additions.alteration;
+        this.mood = additions.mood;
+        this.octave = additions.octave;
+    }
+
+    execute(serviceVariables, config = Note.defaultConfig) {
+        if (!serviceVariables.skip || !serviceVariables.skipCondition.statement) {
+            const commands = [Note.defaultCommands.notes];
+            if (this.mood)
+                this.mood === Note.MOODS.m.name ?
+                    commands.push(Note.defaultCommands.moods) : commands.unshift(Note.defaultCommands.moods);
+            if (this.alteration)
+                this.alteration === Note.ALTERATIONS.b.name ?
+                    commands.push(Note.defaultCommands.alterations) : commands.unshift(Note.defaultCommands.alterations);
+
+            commands.forEach(command => command(this, serviceVariables, config, commands));
+        }
+        else if (this.note === Note.NOTES.F.name && !(this.octave in [Note.OCTAVES[0].name, Note.OCTAVES[4].name, Note.OCTAVES[8].name])) {
+
+        }
+        else if (this.alteration === Note.ALTERATIONS.b.name) {
+            serviceVariables.skip = false;
+        }
+        serviceVariables.iteration++;
+    }
+
+    static defaultCommands = {
+        notes: (noteClass, serviceVariables) => Note.NOTES[noteClass.note](serviceVariables, noteClass.octave),
+        moods: (noteClass, serviceVariables, config) => Note.MOODS[noteClass.mood](serviceVariables, config === Note.defaultConfig ?
+            Note.standartConfig(serviceVariables) : { ...Note.standartConfig(serviceVariables), ...config }),
+        alterations: (noteClass, serviceVariables, _, commands) => Note.ALTERATIONS[noteClass.alteration](serviceVariables, commands)
+    }
+
+    static calc(serviceVariables, octave, operation1, operation2, number) {
+        if (octave) serviceVariables.stack = eval(serviceVariables.stack + operation1 + serviceVariables.stackWithOffset(parseInt(octave)));
+        else serviceVariables.stack = eval(serviceVariables.stack + operation2 + number);
+    }
+
+    static NOTES = {
+        C: (serviceVariables, octave) => {
+            if (!serviceVariables.next
+                || serviceVariables.next === 0)
+                throw new EvalError(`Cannot divide by zero!`);
+            this.calc(serviceVariables, octave, "/", "%", serviceVariables.next);
+        },
+        D: (serviceVariables, octave) => {
+            this.calc(serviceVariables, octave, "-", "-", 1);
+        },
+        E: (serviceVariables, octave) => {
+            this.calc(serviceVariables, octave, "+", "+", 1);
+        },
+        F: (serviceVariables, octave) => {
+            if (octave) Note.OCTAVES[octave](serviceVariables);
+            else Note.skipCondition(serviceVariables, serviceVariables.stack != 0);
+        },
+        G: (serviceVariables, octave) => {
+            this.calc(serviceVariables, octave, "*", "*", 0);
+        },
+        A: (serviceVariables, octave) => {
+            if (octave) serviceVariables.index -= parseInt(octave);
+            else serviceVariables.index -= serviceVariables.stack;
+        },
+        B: (serviceVariables, octave) => {
+            if (octave) serviceVariables.index += parseInt(octave);
+            else serviceVariables.index += serviceVariables.stack;
+        }
+    }
+
+    static ALTERATIONS = {
+        "#": (serviceVariables, commands) => {
+            if (serviceVariables.stack != 0) {
+                serviceVariables.returnTo.push(serviceVariables.iteration);
+                serviceVariables.jumpTo.shift();
+            }
+            else {
+                serviceVariables.jump();
+                commands.splice(0, commands.length);
+            }
+        },
+        "b": (serviceVariables) => {
+            serviceVariables.jumpTo.unshift(serviceVariables.iteration);
+            serviceVariables.return();
+        }
+    }
+
+    static MOODS = {
+        M: (serviceVariables, config) => {
+            serviceVariables.stack = typeof config.inputMethod === "function" ?
+                config.inputMethod(config) : config.inputMethod;
+        },
+        m: (_, config) => {
+            typeof config.outputArgs === "function" ?
+                config.outputMethod(config.outputArgs(config)) : config.outputMethod(config.outputArgs);
+        }
+    }
+
+    static skipCondition(serviceVariables, condition) {
+        if (!condition) {
+            serviceVariables.skipCondition.statement = true;
+        }
+        else {
+
+        }
+    }
+
+    static OCTAVES = {
+        0: (serviceVariables) => {
+
+        },
+        1: (serviceVariables) => {
+            this.skipCondition(serviceVariables, serviceVariables.stack <= serviceVariables.next);
+        },
+        2: (serviceVariables) => {
+            this.skipCondition(serviceVariables, serviceVariables.stack < serviceVariables.next);
+        },
+        3: (serviceVariables) => {
+            this.skipCondition(serviceVariables, serviceVariables.stack == serviceVariables.next);
+        },
+        4: (serviceVariables) => {
+
+        },
+        5: (serviceVariables) => {
+            this.skipCondition(serviceVariables, serviceVariables.stack != serviceVariables.next);
+        },
+        6: (serviceVariables) => {
+            this.skipCondition(serviceVariables, serviceVariables.stack > serviceVariables.next);
+        },
+        7: (serviceVariables) => {
+            this.skipCondition(serviceVariables, serviceVariables.stack >= serviceVariables.next);
+        },
+        8: (serviceVariables) => {
+
+        }
+    }
+
+    static defaultConfig = {
+        outputMethod: undefined,
+        outputArgs: undefined,
+        inputMethod: undefined,
+        inputArgs: undefined
+    }
+
+    static setConfig(config) {
+        Note.standartConfig = (serviceVariables) => ({
+            ...{
+                outputMethod: (outputArgs) => {
+                    outputArgs.output.push(outputArgs.stack);
+                },
+                outputArgs: (config) => config.serviceVariables,
+                inputMethod: (config) => {
+                    return config.serviceVariables.input;
+                },
+                inputArgs: undefined,
+                serviceVariables: serviceVariables
+            }, ...config
+        });
+    }
+
+    static standartConfig(serviceVariables) {
+        return {
+            outputMethod: (outputArgs) => {
+                outputArgs.output.push(outputArgs.stack);
+            },
+            outputArgs: (config) => config.serviceVariables,
+            inputMethod: (config) => {
+                return config.serviceVariables.input;
+            },
+            inputArgs: undefined,
+            serviceVariables: serviceVariables
+        }
+    }
+
+    static findInObject(object, string) {
+        return Object.keys(object).find(elem => elem === string);
+    }
+
+    static checkFormatNote(note, alteration, mood, octave) {
+        if (!note)
+            throw new TypeError(`Note cannot be undefined!`);
+        if (!this.NOTES.hasOwnProperty(note))
+            throw new TypeError(`Note must be in ${Object.keys(this.NOTES)}`);
+        if (alteration && !this.ALTERATIONS.hasOwnProperty(alteration))
+            throw new TypeError(`Alteration must be ${Object.keys(this.ALTERATIONS)[0]} or ${Object.keys(this.ALTERATIONS)[1]}`);
+        if (mood && !this.MOODS.hasOwnProperty(mood))
+            throw new TypeError(`Mood must be ${Object.keys(this.MOODS)[0]} or ${Object.keys(this.MOODS)[1]}`);
+        if (octave && !this.OCTAVES.hasOwnProperty(octave))
+            throw new TypeError(`Octave must be in ${Object.keys(this.OCTAVES)}`);
+    }
+
+    static checkFormatString(noteString) {
+        if (noteString.length === 0 || noteString.length > 4) throw new TypeError(`Сommand "${noteString}" have incorrect format!`);
+
+        if (!this.NOTES.hasOwnProperty(noteString[0]))
+            throw new SyntaxError(`Command "${noteString}" have incorrect syntax! First argument must be in [${Object.keys(this.NOTES)}]!`);
+
+        if (noteString[1] && !(
+            this.ALTERATIONS.hasOwnProperty(noteString[1])
+            || this.MOODS.hasOwnProperty(noteString[1])
+            || this.OCTAVES.hasOwnProperty(noteString[1])))
+            throw new SyntaxError(`Command "${noteString}" have incorrect syntax! Second argument must be in [${Object.keys(this.ALTERATIONS)}] or [${Object.keys(this.MOODS)}] or [${Object.keys(this.OCTAVES)}]`);
+
+        if (noteString[2]) {
+            if (this.ALTERATIONS.hasOwnProperty(noteString[1])) {
+                if (!(
+                    this.MOODS.hasOwnProperty(noteString[2])
+                    || this.OCTAVES.hasOwnProperty(noteString[2])))
+                    throw new SyntaxError(`Command "${noteString}" have incorrect syntax! Third argument must be in [${Object.keys(this.MOODS)}] or [${Object.keys(this.OCTAVES)}]`);
+            }
+            else if (this.MOODS.hasOwnProperty(noteString[1])) {
+                if (!(this.OCTAVES.hasOwnProperty(noteString[2])))
+                    throw new SyntaxError(`Command "${noteString}" have incorrect syntax! Third argument must be in [${Object.keys(this.OCTAVES)}]`);
+            }
+            else throw new TypeError(`Сommand "${noteString}" have incorrect format!`);
+        }
+
+        if (noteString[3]) {
+            if (this.MOODS.hasOwnProperty(noteString[2])) {
+                if (!(this.OCTAVES.hasOwnProperty(noteString[3])))
+                    throw new SyntaxError(`Command "${noteString}" have incorrect syntax! Fourth argument must be in [${Object.keys(this.OCTAVES)}]`);
+            }
+            else throw new TypeError(`Сommand "${noteString}" have incorrect format!`);
+        }
+    }
+
+    static fromString(noteString) {
+        this.checkFormatString(noteString);
+        const note = noteString[0];
+        const alteration = this.findInObject(this.ALTERATIONS, noteString[1]);
+        const mood = this.findInObject(this.MOODS, noteString[1]) || this.findInObject(this.MOODS, noteString[2]);
+        const octave = this.findInObject(this.OCTAVES, noteString[1]) || this.findInObject(this.OCTAVES, noteString[2]) || this.findInObject(this.OCTAVES, noteString[3]);
+        return new Note(note, { alteration: alteration, mood: mood, octave: octave });
+    }
+}
+
+class DoReMi {
+    constructor(notes = [], input = [], run = true) {
+        this.notes = notes;
+        this.serviceVariables = new ServiceVariables(input);
+        if (run) this.run();
+    }
+
+    run() {
+        while (this.serviceVariables.iteration < this.notes.length)
+            this.notes[this.serviceVariables.iteration].execute(this.serviceVariables)
+    }
+
+    get output() {
+        return this.serviceVariables.output;
+    }
+
+    static setConfig(config) {
+        Note.setConfig(config);
+    }
+
+    static convertOutputToString(output) {
+        return String.fromCharCode(...output);
+    }
+
+    static fromString(string, input = [], run = true) {
+        const arr = string.split(/\s+/);
+        const notes = [];
+        arr.forEach(elem => notes.push(Note.fromString(elem)));
+        return new DoReMi(notes, input, run);
+    }
+}
+
+// const serviceVariables = new ServiceVariables();
+
+// const note = Note.fromString("B#m1");
+// note
+// const note2 = new Note("E", { alteration: 'b', mood: 'M' });
+// note2
+// const note3 = Note.fromString("Gm")
+
+// // Note.setConfig({ outputMethod: console.log, outputArgs: (config) => config.serviceVariables.stack });
+// //Note.setConfig({ outputMethod: console.log, outputArgs: serviceVariables.stack });
+
+// serviceVariables
+// note.execute(serviceVariables)//, {outputMethod: console.log, outputArgs: serviceVariables.stack})
+// serviceVariables
+// note2.execute(serviceVariables)//, { inputMethod: 5 })
+// serviceVariables
+// note3.execute(serviceVariables)
+// serviceVariables
+
+// const test = DoReMi.fromString(`B5 E E E A1 E1 D G1 E A1 E2 E1 A3 E3 G4 E Em B2 E1 G0 Em Em2 Am0 Em3 A1 E4 G2 E Em A1 E3 E4 D Dm Bm2 Em3 D1 E3 Em D2 Dm A1 Em Bm2`);
+// console.log(test.output)
+// console.log(DoReMi.convertOutputToString(test.output))
+
+// const test2 = DoReMi.fromString(`BM0 D# Ab0 Bm0`, [100]);
+// console.log(test2.output)
+
+// const test3 = DoReMi.fromString(`BM0 D# Ab0 Bm0`);
+// console.log(test3.output)
+// console.log(DoReMi.convertOutputToString(test3.output))
---
+++ index.js
@@ -0,0 +1,90 @@
+var range = document.getElementById('range');
+
+noUiSlider.create(range, {
+    start: [0, 8],
+    step: 1,
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 8
+    },
+    tooltips: true,
+    format: wNumb({
+        decimals: 0
+    }),
+});
+
+// const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Sampler({
+    urls: {
+        A0: "A0.mp3",
+        C1: "C1.mp3",
+        "D#1": "Ds1.mp3",
+        "F#1": "Fs1.mp3",
+        A1: "A1.mp3",
+        C2: "C2.mp3",
+        "D#2": "Ds2.mp3",
+        "F#2": "Fs2.mp3",
+        A2: "A2.mp3",
+        C3: "C3.mp3",
+        "D#3": "Ds3.mp3",
+        "F#3": "Fs3.mp3",
+        A3: "A3.mp3",
+        C4: "C4.mp3",
+        "D#4": "Ds4.mp3",
+        "F#4": "Fs4.mp3",
+        A4: "A4.mp3",
+        C5: "C5.mp3",
+        "D#5": "Ds5.mp3",
+        "F#5": "Fs5.mp3",
+        A5: "A5.mp3",
+        C6: "C6.mp3",
+        "D#6": "Ds6.mp3",
+        "F#6": "Fs6.mp3",
+        A6: "A6.mp3",
+        C7: "C7.mp3",
+        "D#7": "Ds7.mp3",
+        "F#7": "Fs7.mp3",
+        A7: "A7.mp3",
+        C8: "C8.mp3"
+    },
+    release: 1,
+    baseUrl: "https://tonejs.github.io/audio/salamander/"
+}).toDestination();
+
+function run() {
+    try {
+        const inputArray = document.getElementById("input").value.split(" ");
+        const code = DoReMi.fromString(document.getElementById("code").value, inputArray);
+        if (!document.getElementById("stringOutput").checked)
+            document.getElementById("output").value = code.output.join(" ")
+        else
+            document.getElementById("output").value = DoReMi.convertOutputToString(code.output);
+    }
+    catch (err) {
+        alert(err)
+    }
+}
+
+function toFormat(min, max, number) {
+    if (number < min) number = min;
+    if (number > max) number = max;
+    return number;
+}
+
+function play() {
+    try {
+        const inputArray = document.getElementById("input").value.split(" ");
+        const code = DoReMi.fromString(document.getElementById("code").value, inputArray, false);
+
+        let now = Tone.now();
+        for (let note of code.notes) {
+            synth.triggerAttackRelease(`${note.note}${note.alteration | ""}${note.mood | ""}${note.octave ?
+                toFormat(range.noUiSlider.get()[0], range.noUiSlider.get()[1], note.octave) : "4"}`, "4n", now);
+            now += 0.25;
+        }
+    }
+    catch (err) {
+        alert(err)
+    }
+}
